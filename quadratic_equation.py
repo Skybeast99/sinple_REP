ee05864b7aff3a6df3732dd5c337280bd69a43f9
Skybeast99 <145792205+Skybeast99@users.noreply.github.com>
2023-10-17 19:05:46 +0300

    Add files via upload

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -6,8 +6,16 @@ first = b * b
 second = 4 * a * c
 A = a * 2
 d = first - second
-D = math.sqrt(d)
-x1 = b * -1 + D / A
-x2 = b * -1 - D / A
-print(x1)
-print(x2)
+if d >= 1:
+    D = math.sqrt(d)
+    y1 = b * -1 + D 
+    y2 = b * -1 - D
+    x1 = y1 / A
+    x2 = y2 / A
+    print(x1)
+    print(x2)
+elif d == 0:
+    x1 = b * -1 / A
+    print(x1)
+else:
+    print("корней нет")
