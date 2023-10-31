74b0af0027b310f9115a91a916dd3542a0aa3000
Skybeast99 <145792205+Skybeast99@users.noreply.github.com>
2023-10-31 19:28:33 +0300

    Add files via upload

---
+++ Turtle_random.py
@@ -0,0 +1,14 @@
+from random import*
+from turtle import*
+
+a = 123
+while True:
+    a = randint(1, 180)
+    b = randint(1, 10)
+    c = randint[green, blue, black, orange, red]
+    right(a)
+    forward(b)
+    left(a)
+    forward(b)
+    left(a)
+    color(choice(c))
