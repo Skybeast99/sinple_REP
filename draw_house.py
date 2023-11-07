f74abe5539dda4f7ebb391ea187b1ee97f806958
Skybeast99 <145792205+Skybeast99@users.noreply.github.com>
2023-11-07 16:51:05 +0300

    Add files via upload

--- draw_house.py
+++ draw_house.py
@@ -1,6 +1,10 @@
 import turtle
 
-def draw_base(width, hight, color):
+def draw_base(width, hight, color, x, y):
+
+    turtle.penup()
+    turtle.goto(x, y)
+    turtle.pendown()
 
     turtle.fillcolor(color)
 
@@ -17,8 +21,8 @@ def draw_base(width, hight, color):
 
     turtle.end_fill()
 
-draw_base(10, 200, 'red')
-draw_base(20, 100, 'white')
-draw_base(12, 300, 'green')
-draw_base(15, 150, 'black') 
+draw_base(10, 200, 'red', 100, 150)
+draw_base(20, 100, 'white', 110, 30)
+draw_base(12, 300, 'green', 120, -50)
+draw_base(15, 150, 'black', 70, 80) 
 turtle.done()
