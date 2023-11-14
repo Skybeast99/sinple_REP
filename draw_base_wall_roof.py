8885289f768029763208e14cb37dcaf44b446863
Skybeast99 <145792205+Skybeast99@users.noreply.github.com>
2023-11-14 19:32:07 +0300

    Add files via upload

--- draw_base_wall_roof.py
+++ draw_base_wall_roof.py
@@ -12,6 +12,9 @@ def draw_house(
     wall_w=100,
     wall_h=60,
     wall_c='blue',
+    door_w=20, 
+    door_h=30, 
+    door_c='yellow', 
     roof_w=100,
     roof_h=100, 
     roof_c='red'
@@ -32,6 +35,10 @@ def draw_house(
     wall_h - высота стены
     wall_c - цвет стены
 
+    door_w - ширина стены
+    door_h - высота стены
+    door_c - цвет стены
+
     roof_w - ширина крыши
     roof_h - высота крыши
     roof_c - цвет крыши
@@ -39,6 +46,7 @@ def draw_house(
     print('начинаем стоить дом')
     draw_base(x, y, base_w, base_h, base_c)
     draw_wall(x, y, wall_w, wall_h, wall_c, base_h)
+    draw_door(x, y, door_w, door_h, door_c, base_h, wall_w)
     draw_roof(x, y, roof_w, roof_h, roof_c, base_h, wall_h, wall_w)
 
 
@@ -50,26 +58,33 @@ def draw_base(x, y, width, height, color):
     draw_rectangle(width, height, color)
 
 
-def draw_wall(x, y, width, height, color, base_h):
+def draw_wall(x, y, width, height, color, base_w):
     '''рисует стены'''
     turtle.penup()
-    turtle.goto(x, y + base_h)
+    turtle.goto(x, y + base_w)
     turtle.pendown()
     draw_rectangle(width, height, color)
 
+
+def draw_door(x, y, width, height, color, base_w, wall_w):
+    turtle.penup()
+    turtle.goto(x + wall_w // 2 - width // 2, y + base_w)
+    turtle.pendown()    
+    draw_rectangle(width, height, color)
 
 def draw_roof(x, y, width, height, color, wall_h, base_h, wall_w):
     '''рисует крышу'''
     turtle.penup()
     roof_y = y + base_h + wall_h
-    turtle.goto(x, roof_y)
+    turtle.goto(x + wall_w // 2 - width // 2, roof_y)
     turtle.pendown()
+    turtle.goto(x + wall_w // 2 - width // 2, roof_y)
     turtle.fillcolor(color)
     turtle.begin_fill()
     x_top = x + wall_w // 2
     y_top = y + height + wall_h
     turtle.goto(x_top, y_top)
-    turtle.goto(x_top + wall_w // 2, roof_y)
+    turtle.goto(x_top + width // 2, roof_y)
     turtle.goto(x, roof_y)
     turtle.end_fill()
 
@@ -88,11 +103,11 @@ def  draw_rectangle(width, height, color):
     turtle.end_fill()
 
 
-draw_house()
+draw_house(roof_w=200)
 draw_house(x=50, y=130, base_c='green', wall_c='purple', roof_c='brown')
 draw_house(x=70, y=-140, base_c='orange', wall_c='pink', roof_c='yellow')
 draw_house(x=-198, y=5, base_c='grey', wall_c='black', roof_c='white')
-draw_house(x=-88, y=-100)
+draw_house(x=-100, y=-100)
 draw_house(x=199, y=-19)
 draw_house(x=-250, y=-176)
 
