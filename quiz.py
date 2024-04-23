3de830abddbf875cc02f3a416556f3dc92dff7f2
Skybeast99 <145792205+Skybeast99@users.noreply.github.com>
2024-04-23 19:33:45 +0300

    Add files via upload

--- quiz.py
+++ quiz.py
@@ -1,93 +1,138 @@
-import tkinter
+'''
+TODO:
+    расположить контент вопроса посередине окна
+    расположить кнопки горизонтально
+    начать новую партию
+    картинки к вопросам
+'''
 
+
+import tkinter
 
 questions = [
     {
-        'текст вопроса': 'какой оператор умножает числа в питоне?',
-        'варианты ответов': ['*', '**', 'x', '//'],
-        'индекс правильного ответа': 3
+        'текст вопроса': 'Какой из этих типов данных изменяемый?',
+        'варианты ответов': ['tuple', 'list', 'str', 'int'],
+        'индекс верного ответа': 1
     },
-    {
-        'ВОПРОС': 'какой из этих типов изменяется',
-        'ответы': ["list", "str", "tuple", "int"],
-        'индекс правильного ответа': 0
+    {   'текст вопроса': 'Какой оператор перемножает числа в пайтон?',
+        'варианты ответов': ['x', '//', '*', '**'],
+        'индекс верного ответа': 2
+        
+    },
+    {   'текст вопроса': '?',
+        'варианты ответов': ['', '', '', ''],
+        'индекс верного ответа': 3
     }
-
-
 ]
 
 
 class App:
-    """
-    приложение
-    TODO: окно во весь экран
-    """
+    '''Приложение'''
     def __init__(self) -> None:
         self.window = tkinter.Tk()
         self.window.option_add('*Font', ('Consolas', 30))
         self.width = self.window.winfo_screenwidth()
-        self.hight = self.window.winfo_screenheight()
-        self.window.geometry(f'{self.width}x{self.hight}')
-        self.lable_question_text = None
-        self.lable_answer_1_text = None
-        self.lable_answer_2_text = None
-        self.lable_answer_3_text = None
-        self.lable_answer_4_text = None
+        self.height = self.window.winfo_screenheight()
+        self.window.geometry(f'{self.width}x{self.height}')
+        self.question_number = None
+        self.label_question_text = None
+        self.label_answer_text_1 = None
+        self.label_answer_text_2 = None
+        self.label_answer_text_3 = None
+        self.label_answer_text_4 = None
         self.button_answer_1 = None
         self.button_answer_2 = None
         self.button_answer_3 = None
         self.button_answer_4 = None
+        self.result_frame = None
         self.make_widgets()
-        self.position_widgers()
-        self.make_pack()
+        self.positon_widgets()
         self.start()
         self.window.mainloop()
 
     def make_widgets(self) -> None:
-        '''создает экземпляры виджетов'''
-        self.lable_question_text = tkinter.Label(self.window)
-        self.lable_answer_1_text = tkinter.Label(self.window)
-        self.lable_answer_2_text = tkinter.Label(self.window)
-        self.lable_answer_3_text = tkinter.Label(self.window)
-        self.lable_answer_4_text = tkinter.Label(self.window)
-        self.button_answer_1 = tkinter.Button(self.window)
-        self.button_answer_2 = tkinter.Button(self.window)
-        self.button_answer_3 = tkinter.Button(self.window)
-        self.button_answer_4 = tkinter.Button(self.window)
+        '''Создает экземпляры виджетов'''
+        self.question_frame = tkinter.Frame(self.window)
+        self.label_question_text = tkinter.Label(self.question_frame)
+        self.answers_frame = tkinter.Frame(self.question_frame)
+        self.label_answer_text_1 = tkinter.Label(self.answers_frame)
+        self.label_answer_text_2 = tkinter.Label(self.answers_frame)
+        self.label_answer_text_3 = tkinter.Label(self.answers_frame)
+        self.label_answer_text_4 = tkinter.Label(self.answers_frame)
+        self.buttons_frame = tkinter.Frame(self.question_frame)
 
-    def position_widgers(self) -> None:
-        '''позиционирует виджеты'''
-        self.lable_question_text = tkinter.Label(self.window)
-        self.lable_answer_1_text = tkinter.Label(self.window)
-        self.lable_answer_2_text = tkinter.Label(self.window)
-        self.lable_answer_3_text = tkinter.Label(self.window)
-        self.lable_answer_4_text = tkinter.Label(self.window)
-        self.button_answer_1 = tkinter.Button(self.window, text='1',command=lambda: self.on_click(1))
-        self.button_answer_2 = tkinter.Button(self.window, text='2', command=lambda: self.on_click(2))
-        self.button_answer_3 = tkinter.Button(self.window, text='3', command=lambda: self.on_click(3))
-        self.button_answer_4 = tkinter.Button(self.window, text='4', command=lambda: self.on_click(4))
-    
-    def make_pack(self) -> None:
-        self.lable_question_text.pack()
-        self.lable_answer_1_text.pack()
-        self.lable_answer_2_text.pack()
-        self.lable_answer_3_text.pack()
-        self.lable_answer_4_text.pack()
-        self.button_answer_1.pack()
-        self.button_answer_2.pack()
-        self.button_answer_3.pack()
-        self.button_answer_4.pack()
+        self.button_answer_1 = tkinter.Button(
+            self.buttons_frame, text='1', command=lambda: self.on_click(0)
+        )
+        self.button_answer_2 = tkinter.Button(
+            self.buttons_frame, text='2', command=lambda: self.on_click(1)
+        )
+        self.button_answer_3 = tkinter.Button(
+            self.buttons_frame, text='3', command=lambda: self.on_click(2)
+        )
+        self.button_answer_4 = tkinter.Button(
+            self.buttons_frame, text='4', command=lambda: self.on_click(3)
+        )
+
+    def positon_widgets(self) -> None:
+        '''Позиционирует виджеты в окне'''
+        self.question_frame.pack(expand=True)
+        self.label_question_text.pack(pady=50)
+        self.answers_frame.pack(pady=50)
+        self.label_answer_text_1.pack()
+        self.label_answer_text_2.pack()
+        self.label_answer_text_3.pack()
+        self.label_answer_text_4.pack()
+        self.buttons_frame.pack()
+        self.button_answer_1.pack(side='left', padx=30)
+        self.button_answer_2.pack(side='left', padx=30)
+        self.button_answer_3.pack(side='left', padx=30)
+        self.button_answer_4.pack(side='left', padx=30)
+
+    def show_question(self) -> None:
+        '''Показывает контент вопроса на экране'''
+        question = questions[self.question_number]
+        self.label_question_text['text'] = question['текст вопроса']
+        self.label_answer_text_1['text'] = '1. ' + question['варианты ответов'][0]
+        self.label_answer_text_2['text'] = '2. ' + question['варианты ответов'][1]
+        self.label_answer_text_3['text'] = '3. ' + question['варианты ответов'][2]
+        self.label_answer_text_4['text'] = '4. ' + question['варианты ответов'][3]
+
+    def on_click(self, button_index: int) -> None:
+        question = questions[self.question_number]
+        if button_index == question['индекс верного ответа']:
+            self.correct_answers += 1
+        else:
+            self.incorrect_answers += 1
+        self.question_number += 1
+        # Закончить викторину
+        if len(questions) == self.question_number:
+            self.show_result()
+        else:
+            self.show_question()
+
+    def show_result(self) -> None:
+        '''Показывает на экране результат викторины'''
+        self.question_frame.pack_forget()
+        self.result_frame = tkinter.Frame(self.window)
+        self.result_frame.pack()
+        tkinter.Label(self.result_frame, text='Викторина завершена!').pack()
+        tkinter.Label(self.result_frame, text=f'Всего вопросов: {len(questions)}').pack()
+        tkinter.Label(self.result_frame, text=f'Правильных ответов: {self.correct_answers}').pack()
+        tkinter.Label(self.result_frame, text=f'Ошибок: {self.incorrect_answers}').pack()
+        tkinter.Button(self.result_frame, text='нчать заново', command=self.start).pack()
 
     def start(self) -> None:
         self.question_number = 0
-        self.lable_question_text['text'] = questions[self.question_number]['текст вопроса']
-        self.lable_answer_1_text['text'] = '1. ' + questions[self.question_number]['варианты ответов'][0]
-        self.lable_answer_2_text['text'] = '2. ' + questions[self.question_number]['варианты ответов'][1]
-        self.lable_answer_3_text['text'] = '3. ' + questions[self.question_number]['варианты ответов'][2]
-        self.lable_answer_4_text['text'] = '4. ' + questions[self.question_number]['варианты ответов'][3]
+        self.correct_answers = 0
+        self.incorrect_answers = 0
+        if self.result_frame:
+            self.result_frame.pack_forget()
+        self.question_frame.pack(expand=True)
+        self.show_question()
 
-    def on_click(self, button_number: int) -> None:
-        print('кнопка нажата', button_number)
-        # TODO: сравнить номер кнопки с индексом правильного ответа
 
 App()
+
+# self.question_frame['bg'] = 'coral'
